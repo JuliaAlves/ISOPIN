67be416cfc9bae18d88b156690229b1ff4ea612f
Guilherme Brandt <gui.g.brandt@gmail.com>
2017-05-13 19:45:12 -0300

    Trigger do cache arrumada (era tão simples...)

--- ServerSide/cache.sql
+++ ServerSide/cache.sql
@@ -8,44 +8,40 @@ CREATE TABLE IF NOT EXISTS interaction_cache (
 
 $$
 
--- Trigger para limpar o cache de todas as proteínas relacionadas a qualquer 
--- interactoma que seja alterado no banco de dados
-CREATE TRIGGER tg_updateCacheForInteractome AFTER UPDATE ON interactome FOR EACH ROW
+-- Procedure para limpar todas as linhas do cache relacionadas a uma proteína
+-- específica
+CREATE PROCEDURE clearCacheForLocus(
+  l VARCHAR(11)
+)
 BEGIN
-	DECLARE interacoes TEXT;
-	DECLARE locusAtual VARCHAR(11);
-	DECLARE i INT DEFAULT 0;
-	DECLARE caractereAtual CHAR;
-	DECLARE tamanho INT DEFAULT 0;
-    DECLARE done BOOL;
-        
-	DECLARE cur CURSOR FOR SELECT interactions FROM interaction_cache WHERE locus = OLD.locusA OR locus = OLD.locusB;
-	DECLARE CONTINUE HANDLER FOR NOT FOUND SET done := TRUE;
-	
-    
-	OPEN cur;
-	interactionsLoop: LOOP
-		FETCH cur INTO interacoes;
-		
-		WHILE i < LEN(interacoes) DO
-			SET tamanho = tamanho + 1;
-			SET caractereAtual := SUBSTRING(interacoes, i, 1);
-			IF caractereAtual = ',' THEN			
-				SET locusAtual := SUBSTRING(interacoes, i, tamanho);
-				DELETE FROM interaction_cache WHERE locus = locusAtual;
-				SET tamanho := 0;
-			END IF;
-			
-			SET i := i + 1;
-		END WHILE;
-		
-		IF done THEN
-			LEAVE interactionsLoop;
-		END IF;
-	END LOOP interactionsLoop;
-	CLOSE cur;
-	
-	DELETE FROM cache WHERE locus = OLD.locusA OR locus = OLD.locusB;
+  DELETE FROM interaction_cache WHERE locus = l OR interactions LIKE CONCAT('%', l, '%');
+END;
+
+$$
+
+-- Triggers para limpar o cache de todas as proteínas relacionadas a qualquer 
+-- interactoma que seja alterado no banco de dados
+CREATE TRIGGER tg_updateCacheForInteractomeOnInsert AFTER INSERT
+ON interactome FOR EACH ROW
+BEGIN
+  CALL clearCacheForLocus(NEW.locusA);
+  CALL clearCacheForLocus(NEW.locusB);
+END;
+
+CREATE TRIGGER tg_updateCacheForInteractomeOnUpdate AFTER UPDATE
+ON interactome FOR EACH ROW
+BEGIN
+  CALL clearCacheForLocus(OLD.locusA);
+  CALL clearCacheForLocus(OLD.locusB);
+  CALL clearCacheForLocus(NEW.locusA);
+  CALL clearCacheForLocus(NEW.locusB);
+END;
+
+CREATE TRIGGER tg_updateCacheForInteractomeOnDelete AFTER DELETE
+ON interactome FOR EACH ROW
+BEGIN
+  CALL clearCacheForLocus(OLD.locusA);
+  CALL clearCacheForLocus(OLD.locusB);
 END;
 
 $$
